d28640eceb5c7ce3f39da1be2a77564189a95e2d
rupert.avery <rupert.avery@gmail.com>
2013-05-30 08:10:27 +0800

    Slight API change, code cleanup

---
+++ ExpressionCompiler.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq.Expressions;
+
+namespace ExpressionEvaluator
+{
+    public abstract class ExpressionCompiler
+    {
+        protected Expression Expression = null;
+        protected Parser Parser = null;
+        protected TypeRegistry TypeRegistry = new TypeRegistry();
+        protected string Pstr = null;
+
+        public string StringToParse
+        {
+            get { return Parser.StringToParse; } 
+            set { Parser.StringToParse = value; }
+        }
+
+        public void RegisterDefaultTypes()
+        {
+            TypeRegistry.RegisterDefaultTypes();
+        }
+
+        public void RegisterType(string key, object type)
+        {
+            TypeRegistry.Add(key, type);
+        }
+
+        public Expression BuildTree()
+        {
+            return Parser.BuildTree();
+        }
+
+        public void Parse()
+        {
+            Parser.Parse();
+        }
+
+    }
+}
