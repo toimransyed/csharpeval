11682f2e1cc2ffb36ac45c9e6b878e1254005a80
RupertAvery <rupert.avery@gmail.com>
2014-03-02 19:44:57 +0800

    Merged MethodHandling and Call branches

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -120,6 +120,11 @@ namespace Tests
         {
             X += value;
         }
+
+        public int Bar(int value)
+        {
+            return value * 2;
+        }
     }
 
 
@@ -139,11 +144,17 @@ namespace Tests
             var a = scope.data.Value() && scope.item.Value();
             //var b = !scope.data.Value() || scope.item.Value();
 
+            System.Threading.Thread.CurrentThread.CurrentCulture = new CultureInfo("fr-FR");
+            var pi = Convert.ToString(3.141592654);
+            var xs = 2d;
+            var pipi = 3.141592654.ToString();
+            var c0 = new CompiledExpression() { StringToParse = "3.141592654.ToString()" };
+            var pi2 = c0.Eval();
 
             var p = scope.x[0];
 
             // (data.Value && !item.Value) ? 'yes' : 'no'
-            var c = new CompiledExpression() { StringToParse = "data.Foo(data.Foo(10))" };
+            var c = new CompiledExpression() { StringToParse = "data.Foo(30 + data.Bar(10))" };
             c.RegisterType("data", data);
             Console.WriteLine(data.X);
             c.Call();
@@ -161,6 +172,25 @@ namespace Tests
             Console.WriteLine(scope.data.X);
             f2(scope);
             Console.WriteLine(scope.data.X);
+
+            scope.c = new c();
+
+            var c3 = new CompiledExpression() { StringToParse = "c.sum(1,2,3,4,5,6,7,8)" };
+            var f3 = c3.ScopeCompile();
+            var x3 = f3(scope);
+
+            var c4 = new CompiledExpression() { StringToParse = "c.sum(1,2)" };
+            var f4 = c4.ScopeCompile();
+            var x4 = f4(scope);
+
+            var c5 = new CompiledExpression() { StringToParse = "c.sum(1.0d,2.0d)" };
+            var f5 = c5.ScopeCompile();
+            var x5 = f5(scope);
+
+            var c6 = new CompiledExpression() { StringToParse = "c.sum(1,2.0d)" };
+            var f6 = c6.ScopeCompile();
+            var x6 = f6(scope);
+
 
             Console.ReadLine();
         }
